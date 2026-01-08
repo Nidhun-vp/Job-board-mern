3353d08ac193fd5d6b6347607106b40c3fb5104e
NidhuN <100463732+Nidhun-vp@users.noreply.github.com>
2026-01-08 12:31:04 +0530

    Update server.js

--- job-board-mern/server/server.js
+++ job-board-mern/server/server.js
@@ -16,8 +16,11 @@ app.use("/api/jobs", jobRoutes);
 const PORT       = process.env.PORT || 5000;
 const MONGO_URL  = process.env.MONGO_URI;
 
-mongoose
-  .connect(MONGO_URL)
+mongoose 
+  .connect(MONGO_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log(" Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
